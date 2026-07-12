aae174b1bb569fe1b2b6071f40c87a7bf8778a2a
John Pasea <johnpasea01@gmail.com>
2026-07-12 21:19:19 +0300

    Implement user registration with bcrypt authentication

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
 
 const authController = require("../controllers/authController");
 
+// Registration
 router.post("/register", authController.register);
 
 module.exports = router;
